8f695e8f84d315307da071a3a619702e25b6569a
mazimi surface <mohammad.azimi.1996@gmail.com>
2024-08-26 14:33:02 +0330

    dbcontext added

---
+++ DotNetLinkShortener/Data/LinkShortnerDbContext.cs
@@ -0,0 +1,13 @@
+﻿using DotNetLinkShortener.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetLinkShortener.Data;
+
+public class LinkShortnerDbContext : DbContext
+{
+    public LinkShortnerDbContext(DbContextOptions<LinkShortnerDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Link> Links { get; set; }
+}
