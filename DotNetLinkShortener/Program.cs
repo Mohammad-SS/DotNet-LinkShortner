ec1a26bc181f2d05646bb13f10c1046d232ec81f
mazimi surface <mohammad.azimi.1996@gmail.com>
2024-08-26 16:51:37 +0330

    connected to psql

--- DotNetLinkShortener/Program.cs
+++ DotNetLinkShortener/Program.cs
@@ -1,9 +1,13 @@
+using DotNetLinkShortener.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddControllers();
+builder.Services.AddDbContext<LinkShortnerDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -12,6 +16,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MapControllers();
 
 app.UseHttpsRedirection();
 
