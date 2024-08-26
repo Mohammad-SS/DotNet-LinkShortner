0209bea32338fd899ea9eb5e3de0a084dd89ec2b
mazimi surface <mohammad.azimi.1996@gmail.com>
2024-08-26 14:29:04 +0330

    Links models Added

---
+++ DotNetLinkShortener/Models/Link.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DotNetLinkShortener.Models;
+
+public class Link
+{
+    [Key] public int Id { get; set; }
+
+    [Required] [MaxLength(2000)] public string TargetUrl { get; set; }
+
+    [Required] [MaxLength(10)] public string Slug { get; set; }
+
+    public DateTime Created { get; set; } = DateTime.Now;
+    public DateTime Modified { get; set; } = DateTime.Now;
+    
+}
